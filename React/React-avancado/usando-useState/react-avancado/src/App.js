686b989e9c5a3c49c6ffb002877204e1d97110ce
sxndev <sxnsilva@gmail.com>
2026-01-13 01:44:00 -0300

    aula de react adicionada

---
+++ React/React-avancado/usando-useState/react-avancado/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DeckOfCards from './components/deck-of-cards/deck-of-cards';
+
+function App() {
+
+  return (
+    <>
+      <DeckOfCards />
+    </>
+  )
+}
+
+export default App;
